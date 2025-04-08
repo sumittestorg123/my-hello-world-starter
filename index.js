6ccec9fc9c4f073595988a7babe7080d9e2355e3
Sumit Madan <sumitalwayssmiling@gmail.com>
2025-04-08 12:19:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end('Hello World..');
 });
 
 server.listen(port, () => {
